0f55008a5fcc751c39b1a9a02e51d56d0959a2cc
glc-omid <oeidivandi@lacentrale.fr>
2024-12-13 00:32:07 +0100

    init commit

---
+++ packages/package-b/types.ts
@@ -0,0 +1,12 @@
+
+export type SimpleType = string | number;
+
+export type ComplexType = {
+  name: string;
+  value: number;
+};
+
+export interface InterfaceType {
+  name: string;
+  value: number;
+}
